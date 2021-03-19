d024c91b4403b894687e92294edcf4c9c709948f
Stefanie Dziadkowiec <stefaniemarie.dz@gmail.com>
2021-03-19 16:09:16 +0100

    starting to make a toggle switch for the theme

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,6 +4,10 @@ import styled from 'styled-components'
 const Header = () => {
   return (
     <HeaderBase>
+      <label className="switch">
+        <input type="checkbox" />
+        <span className="switch" />
+      </label>
       <h1>OMDB Library</h1>
     </HeaderBase>
   )
@@ -12,6 +16,13 @@ const Header = () => {
 const HeaderBase = styled.header`
   text-align: center;
   color: ${(props) => props.theme.fontColor};
+  position: relative;
+
+  label {
+    position: absolute;
+    left: 10px;
+  }
+
   h1 {
     font-size: ${(props) => props.theme.fontSize[3]};
     padding: ${(props) => props.theme.spacing[2]};
