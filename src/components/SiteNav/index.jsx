bd3b8bfe1b8cf195f87b2225078a980c79c9f10c
Stefanie Dziadkowiec <stefaniemarie.dz@gmail.com>
2021-03-20 11:03:28 +0100

    started select dropdown

--- src/components/SiteNav/index.jsx
+++ src/components/SiteNav/index.jsx
@@ -6,7 +6,10 @@ const SiteNav = () => {
   return (
     <NavBase>
       <NavLink className="nav-item" to="/">
-        Movies & Series
+        <select name="media-type">
+          <option value="movies">Movies</option>
+          <option value="series">Series</option>
+        </select>
       </NavLink>
       <NavLink className="nav-item" to="/mywatchlist">
         My Watch List
@@ -21,10 +24,16 @@ const NavBase = styled.nav`
   justify-content: center;
   gap: ${(props) => props.theme.spacing[4]};
 
-  .nav-item {
+  .nav-item,
+  select {
     text-decoration: none;
     color: ${(props) => props.theme.fontColor};
     font-size: ${(props) => props.theme.fontSize[1]};
+  }
+
+  select {
+    border: none;
+    background-color: transparent;
   }
 `
 
